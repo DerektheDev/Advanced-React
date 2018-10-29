05dbc4f2d3ffd08166af735761d86f6ff5e0dede
Derek Montgomery <derekm@ifit.com>
2018-10-29 11:26:58 -0500

    Broken at the end of vid #12

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,7 @@
-const Query = {};
+const Query = {
+  // dogs(parent, args, ctx, info) { // dogs:function()
+  //   return [{name: 'Snickers'}, {name: 'Sunny'}];
+  // }
+};
 
 module.exports = Query;
