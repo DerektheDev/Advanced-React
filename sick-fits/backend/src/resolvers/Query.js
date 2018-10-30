50fb8afa5a4d4992e632e4105ce8c6a049277721
Derek Montgomery <derekm@ifit.com>
2018-10-30 16:25:49 -0500

    End of vid #14

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,7 +1,8 @@
 const Query = {
-  // dogs(parent, args, ctx, info) { // dogs:function()
-  //   return [{name: 'Snickers'}, {name: 'Sunny'}];
-  // }
+  dogs(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  }
 };
 
 module.exports = Query;
